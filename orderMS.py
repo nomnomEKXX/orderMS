b964c6e94cbd29727f4288bd4d8df4403db15b70
Xue Zhen Ng <xuezhen.ng.2020@scis.smu.edu.sg>
2022-03-21 21:19:24 +0800

    changed the data formate of post request of order to firebase
    Signed-off-by: Xue Zhen Ng <xuezhen.ng.2020@scis.smu.edu.sg>

--- orderMS.py
+++ orderMS.py
@@ -6,7 +6,6 @@ from firebase_admin import firestore
 
 from flask_cors import CORS, cross_origin 
 
-
 # Use the application default credentials
 
 cred = credentials.Certificate("serviceAccountKey.json")
@@ -16,108 +15,66 @@ db = firestore.client()
 app = Flask(__name__)
 CORS(app)
 
+# this is not dynamic yet 
 @app.route("/orders", methods=["GET"]) 
 @cross_origin()
 def getOrders():
-    orders = db.collection('orders').document('weCssyTvRI5bJT2IHqOa').get()
+    orders = db.collection('orders').document('Qt1UfT9hExtldgn2qsxl').get()
     return orders.to_dict()
 
+# this creates a empty document first newOrderDoc, then fills up the details with the orderData params 
 @app.route("/order", methods=["GET","POST"])
 @cross_origin()
 def createOrder():
     orderData = request.get_json() 
     newOrderDoc = db.collection('orders').document()
     newOrderDoc.set(
-        {
-    "buyerID" : orderData['buyerID'],
-    "sellerID" : orderData['sellerID'],
-    "cart" : orderData['cart']
+        {   
+    # "buyerID" : orderData['buyerID'],
+    # "sellerID" : orderData['sellerID'],
+    # "status": orderData['status'],
+    # "cart" : orderData['cart'],
+    # "collectionTime" : orderData['collectionTime']
+
+    "status": orderData['status'],
+    "uid" : orderData['UID'],
+    "subtotal": orderData['subtotal'],
+    "storeID" : orderData['storeID'],
+    "collectionTime" : orderData['collectionTime'],
+     "cart" : {
+        orderData['foodName']: {
+        "counter" : orderData['counter'],
+        "foodDesc" : orderData['foodDesc'],
+        "foodName" : orderData['foodName'],
+        "image": orderData['image'],
+        "oldPrice": orderData['oldPrice'],
+        "price": orderData['newPrice'],
+        "quantity": orderData['quantity'],
+        "shopKey": orderData['storeID'] 
+        }
+    }
 }
     )
     return 'order added to firebase successfully'
+
+# this updates the orders by taking in the orderID and the new status to update 
+@app.route("/orderUpdate", methods=["GET","POST"])
+@cross_origin()
+def updateOrder():
+    orderToUpdate = request.get_json()
+    order = db.collection('orders').document(orderToUpdate['orderID'])
+    order.update({
+        'status': orderToUpdate['status']
+    })
+    return "order status updated successfully"
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
 
 
 
-# @app.route("/orders")
-# def get_all():
-#     booklist = Book.query.all()
-#     if len(booklist):
-#         return jsonify(
-#             {
-#                 "code": 200,
-#                 "data": {
-#                     "books": [book.json() for book in booklist]
-#                 }
-#             }
-#         )
-#     return jsonify(
-#         {
-#             "code": 404,
-#             "message": "There are no books."
-#         }
-#     ), 404
-
-
-# ---------------------------------------------------------------------------
-
-# from flask import Flask, request, jsonify, render_template
-
-# import firebase_admin
-# from firebase_admin import firestore
-# from firebase_admin import credentials
-
-# cred = credentials.Certificate("serviceAccountKey.json")
-# firebase_admin.initialize_app(cred)
-# db = firestore.client()
-
-# app = Flask(__name__)
-
-# @app.route("/addbuyer", methods=["POST", "GET"])
-# def home():
-#     if request.method == "POST":
-#         pass
-#     return render_template('home.html')
-    
-
-# @app.route("/buyers")
-# def get_all_buyer():
-#     string = ""
-#     buyer_doc = db.collection('persons').stream()
-#     for doc in buyer_doc:
-#         #return jsonify(doc.to_dict()), 200
-#         string += str(doc.to_dict())
-#         string += "\n"
-#     return string
-        
-    
-
-# @app.route("/add/<string:buyerName>/<string:buyerID>", methods=["POST", "GET"])
-# def add_buyer(buyerName, buyerID):
-#     db.collection('persons').add({'name':buyerName,'age':buyerID})
-#     return "working"
-
-
-# if __name__ == "__main__":
-#     app.run(port=5000, debug=True)
 
 
 
 
-
-
-# # import firebase_admin
-# from firebase_admin import credentials
-# from firebase_admin import db
-
-# # Fetch the service account key JSON file contents
-# cred = credentials.Certificate('nomnom-db-firebase-adminsdk-bhktu-52a40c9f91.json')
-# # Initialize the app with a service account, granting admin privileges
-# firebase_admin.initialize_app(cred, {
-#     'databaseURL': "https://nomnom-db-default-rtdb.asia-southeast1.firebasedatabase.app"
-# })
-
-# ref = db.reference('Database reference')
-# print(ref.get())
